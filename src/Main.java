2ecede524ae7a934cc171e28694b23b41212f1ca
ysbllcby <ysabellacubay@gmail.com>
2023-05-14 14:18:39 -0600

    Leetcode 198.

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+        //Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+        //Solve using dynamic programming
+        int[] nums = {1,2,3,1};
+        System.out.println(rob(nums));
+
+
+    }
+
+    private static int rob(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+
+        if(nums.length == 1){
+            return dp[0];
+        }
+
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        if(nums.length == 2){
+            return dp[1];
+        }
+
+        for(int i = 2; i < nums.length; i++){
+            dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i]);
+        }
+
+        return dp[nums.length-1];
+    }
+}
